fb8e5399586fff1d06d5b9218d27f9ed85e74eca
quangbach1709 <bachh1124@edumail365.onmicrosoft.com>
2026-01-05 22:30:50 +0700

    finish dang ky khuon mat

--- backend/src/main/java/com/hrm/backend/dto/FaceEmbeddingDto.java
+++ backend/src/main/java/com/hrm/backend/dto/FaceEmbeddingDto.java
@@ -2,7 +2,7 @@ package com.hrm.backend.dto;
 
 import com.hrm.backend.entity.FaceEmbedding;
 
-public class FaceEmbeddingDto extends AuditableDto{
+public class FaceEmbeddingDto extends AuditableDto {
     private PersonDto person;
     private double[] embeddingVector;
     private FileDescriptionDto imageUrl;
@@ -16,14 +16,22 @@ public class FaceEmbeddingDto extends AuditableDto{
     public FaceEmbeddingDto(FaceEmbedding entity) {
         super(entity);
         this.person = new PersonDto(entity.getPerson(), false);
-        this.embeddingVector = entity.getEmbeddingVector();
         this.imageUrl = new FileDescriptionDto(entity.getImageUrl());
         this.isActive = entity.isActive();
         this.modelVersion = entity.getModelVersion();
     }
 
+    public FaceEmbeddingDto(FaceEmbedding entity, Boolean loadAll) {
+        super(entity);
+        this.person = new PersonDto(entity.getPerson(), loadAll);
+        this.imageUrl = new FileDescriptionDto(entity.getImageUrl());
+        this.isActive = entity.isActive();
+        this.modelVersion = entity.getModelVersion();
+        this.embeddingVector = entity.getEmbeddingVector();
+    }
+
     public PersonDto getPerson() {
-        
+
         return person;
     }
 
